977dac11d44a56ca18dd0f8b5a9f5338a9683626
flukekd <57992223+flukekd@users.noreply.github.com>
2023-05-21 03:45:58 +0700

    improve FeedForward

--- model/model.py
+++ model/model.py
@@ -104,8 +104,9 @@ class FeedForward(nn.Module):
     def __init__(self, n_embd):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Linear(n_embd, n_embd),
+            nn.Linear(n_embd, 4*n_embd),
             nn.ReLU(),
+            nn.Linear(4*n_embd, n_embd),
         )
 
     def forward(self, x):
